0c82ab73c6395ba9adc706f810ff5801386ee4bb
Jeremy Wang <jeremywang512@gmail.com>
2022-10-30 16:18:05 +0800

    Added comments and tweaked some redundancies

--- FarmLot.java
+++ FarmLot.java
@@ -1,10 +1,16 @@
 import java.util.ArrayList;
 
+/** This class represents a farm lot. A farm lot consists of a number of tiles and a player.
+  */
 public class FarmLot {
     private ArrayList<Tile> tiles;
     private Player player;
 
-    // Constructors
+    /** Creates a FarmLot object by supplying the number of tiles that will be on the farm. It also
+      * constructs the appropriate number of tiles and a player.
+      * 
+      * @param farmSize number of tiles
+      */
     public FarmLot(int farmSize) {
         this.tiles = new ArrayList<Tile>();
         for (int i = 0; i < farmSize; i++) {
@@ -13,23 +19,37 @@ public class FarmLot {
         this.player = new Player();
     }
 
-    // Getters
+    /** This method returns the list of tiles that are on the farm.
+      * 
+      * @return list of tiles
+      */
     public ArrayList<Tile> getTiles() {
         return this.tiles;
     }
 
+    /** This method returns the info of the player
+      * 
+      * @return info of the player
+      */
     public Player getPlayer() {
         return this.player;
     } 
 
-    // Methods
+    /** This method checks if the player has sufficient amount of coins to use the desired tool and
+      * subtracts the amount from the player's wallet. It then performs the desired action of the given
+      * tool and adds the appropriate amount of experience to the player's experience.
+      * 
+      * @param toolIndex    index of the desired tool
+      * @param tileIndex    index of the tile to perform action on
+      * @return returns true if the player has sufficient amount of coins to use tool and false if otherwise
+      */
     public boolean useTool(int toolIndex, int tileIndex) {
         if (this.player.subtractCoins(player.tools.get(toolIndex).getToolCost())) {
             this.player.addExperiencePoints(player.tools.get(toolIndex).getExpGain());
 
             switch (this.player.tools.get(toolIndex).getName()) {
                 case "Plow":
-                    this.tiles.get(tileIndex).plowTile();
+                    this.tiles.get(tileIndex).setPlowed(true);
                     System.out.println("Successfully plowed the tile.");
                     break;
             
@@ -49,7 +69,7 @@ public class FarmLot {
                     break;
 
                 case "Shovel":
-                    this.tiles.get(tileIndex).removeWitheredCrop();
+                    this.tiles.get(tileIndex).resetTile();
                     System.out.println("Successfully removed the withered crop.");
                     break;
                 
@@ -67,6 +87,12 @@ public class FarmLot {
 
     }
 
+    /** This method checks if the player has sufficient amount fo coins to plant the desired crop and will
+      * plant the crop on the given tile.
+      * 
+      * @param crop         crop to be planted on the tile
+      * @param tileIndex    index of the tile to plant the crop on
+      */
     public void plantCrop(Crop crop, int tileIndex) {
         if(this.player.subtractCoins(crop.getBaseSellPrice() - this.player.farmerType.get(this.player.getCurrFarmerType()).getSeedCostReduction())) {
             this.tiles.get(tileIndex).setCrop(crop);
@@ -77,6 +103,12 @@ public class FarmLot {
         }
     }
 
+    /** This method calculates the amount that the player will receive after harvesting the tile and adds the
+      * amount to the player's wallet. It also gives the player the appropriate amount of experience that the crop
+      * gives when harvested. It will finally reset the tile to its default state.
+      * 
+      * @param tileIndex    index of the tile where the crop is to be harvested
+      */
     public void harvestCrop(int tileIndex) {
         int temp = this.tiles.get(tileIndex).getCrop().harvestAmt();
         int harvestTotal = temp * (this.tiles.get(tileIndex).getCrop().getBaseSellPrice() + this.player.farmerType.get(this.player.getCurrFarmerType()).getBonusProduceEarnings());
@@ -90,18 +122,16 @@ public class FarmLot {
         System.out.println("Turnips Produced: " + temp);
         System.out.println("Objectcoins Obtained: " + finalHarvestPrice);
 
-        this.tiles.get(tileIndex).getCrop().setWaterAmt(0);
-        this.tiles.get(tileIndex).getCrop().setFertilizerAmt(0);
-        this.tiles.get(tileIndex).getCrop().setDaysPlanted(0);
-        this.tiles.get(tileIndex).setPlowed(false);
-        this.tiles.get(tileIndex).setOccupied(false);
-        this.tiles.get(tileIndex).setCrop(null);
-
+        this.tiles.get(tileIndex).resetTile();
     }
 
+    /** This method harvests all the crops that are ready to be harvested in the farm.
+      */
     public void harvestAll() {
         for (int i = 0; i < this.tiles.size(); i++) {
-            harvestCrop(i);
+            if (this.tiles.get(i).getCrop().isReady()) {
+                harvestCrop(i);
+            }
         }
     }
 
